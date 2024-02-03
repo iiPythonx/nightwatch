4e29f023057d789bb14af366edbcff33bc215210
iiPython <ben@iipython.dev>
2024-02-03 14:29:10 -0600

    Add a urwid package check so I can publish to PyPI

--- nightwatch/__main__.py
+++ nightwatch/__main__.py
@@ -3,6 +3,20 @@
 # Initialization
 def main() -> None:
 
+    # Check urwid
+    def invalid_urwid_distrib():
+        exit("ERROR: Nightwatch requires urwid to have a specific unicode patch not currently included in upstream urwid.\n\n" \
+            "To fix this, run:\npip install -U urwid@git+https://github.com/iiPythonx/urwid\n\n" \
+            "and relaunch Nightwatch.")
+
+    try:
+        from urwid import is_iipython_urwid
+        if not is_iipython_urwid:
+            invalid_urwid_distrib()
+
+    except ImportError:
+        invalid_urwid_distrib()
+
     # Modules
     from argparse import ArgumentParser
     from nightwatch.client import start_client
