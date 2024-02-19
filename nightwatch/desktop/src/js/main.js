e5653356e6b5e101aa8a3f427f7d116f872d697d
iiPython <ben@iipython.dev>
2024-02-18 18:11:30 -0600

    Full Markdown/Emoji support in the desktop app

--- nightwatch/desktop/src/js/main.js
+++ nightwatch/desktop/src/js/main.js
@@ -1,5 +1,8 @@
 // Copyright (c) 2024 iiPython
 
+// Setup main store (for future use)
+const store = new Store(".nightwatch.dat");
+
 // General UI handler
 class UIStateManager {
     constructor() {
@@ -19,20 +22,46 @@ const ui = new UIStateManager();
 const notifier = new AWN({ icons: { enabled: false } });
 const notificationAudio = new Audio("../audio/notification.mp3");
 
+// HTML prevention in marked
+const preprocess = (markdown) => {
+    return markdown
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+};
+marked.use({ hooks: { preprocess } });
+
+// Setup emoji
+const emoji = new EmojiConvertor();
+emoji.replace_mode = "css";
+emoji.use_sheet = true;
+emoji.img_set = "twitter";
+emoji.img_sets.twitter.sheet = "./emoji/sheet_twitter_64.png";
+
 // Handle message processing
 const appWindow = window.__TAURI__.window.appWindow;
 const container = $("#messages-container");
 async function process_message(message) {
     if (!message.user) message.user = { name: "Nightwatch", color: "gray" };
-    const latest = $(`<li class = "list-group-item"><span style = "color: ${message.user.color}">${message.user.name}</span>: ${message.text}</li>`)[0];
-    container.append(latest);
+
+    // Render the message (Markdown + Santization + Emoji)
+    message.text = DOMPurify.sanitize(emoji.replace_colons(marked.parse(message.text)), { USE_PROFILES: { html: true } });
+
+    let latest;
+    if (window._last_user !== message.user.name) {
+        latest = $(`<li class = "list-group-item"><span style = "color: ${message.user.color}">${message.user.name}</span>: ${message.text}</li>`);
+        container.append(latest);
+        window._last_user = message.user.name;
+    } else {
+        latest = $(message.text);
+        container.children().last().append(latest);
+    }
+    latest.find("a").attr("target", "_blank");
 
     // Handle autoscrolling
-    const newMessageHeight = latest.offsetHeight + parseInt(getComputedStyle(latest).marginBottom);
+    const newMessageHeight = latest[0].offsetHeight + parseInt(getComputedStyle(latest[0]).marginBottom);
     const scrollOffset = container[0].scrollTop + container[0].offsetHeight;
-    if (container[0].scrollHeight - newMessageHeight <= scrollOffset + 10) {
-        container[0].scrollTop = container[0].scrollHeight;
-    }
+    if (container[0].scrollHeight - newMessageHeight <= scrollOffset + 10) container[0].scrollTop = container[0].scrollHeight;
 
     // Handle notifications
     if (!(await appWindow.isFocused())) notificationAudio.play();
