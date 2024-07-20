57ad9758af3b104bedc216908e8af284d7b62c7f
iiPython <ben@iipython.dev>
2024-07-19 20:46:57 -0500

    readd missing constants file

---
+++ nightwatch/server/utils/constant.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2024 iiPython
+
+# Modules
+from nightwatch import __version__
+from nightwatch.config import config
+
+# Constants
+class Constant:
+    SERVER_USER: dict[str, str] = {"name": "Nightwatch", "color": "gray"}
+    SERVER_NAME: str = config["server.name"] or "Untitled Server"
+    SERVER_VERSION: str = __version__
